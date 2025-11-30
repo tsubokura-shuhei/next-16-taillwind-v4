4410ec2034eadd0e0d930296d31d6ff40d2d679a
tsubokura-shuhei <repeat2639@gmail.com>
2025-11-30 23:36:43 +0900

    file add

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -5,6 +5,8 @@ export default function Home() {
     <div className="text-clamp-[920,20,30,max]">
       Hello World
     </div>
+    <div className="btn">ボタン</div>
+    <div className="w-clamp-[920,260,400] h-clamp-[920,260,400] bg-amber-400 ml-clamp-[920,20,40]"></div>
     </>
   );
 }
